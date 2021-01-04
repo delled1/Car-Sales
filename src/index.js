a70cfbaa8d1179896505f97855190dddf1c4165d
Eric Della <ericdella13@gmail.com>
2021-01-04 11:28:38 -0800

    created redux store, added {Provider}

--- src/index.js
+++ src/index.js
@@ -1,9 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
-
+import {createStore} from "redux";
+import{Provider} from "react-redux"
+import {featureReducer} from "./reducers/index"
 import 'bulma/css/bulma.css';
 import './styles.scss';
 
+//create Redux store
+const store = createStore(featureReducer);
+
+//Provide the Redux store to all components
 const rootElement = document.getElementById('root');
-ReactDOM.render(<App />, rootElement);
+ReactDOM.render(
+
+<Provider store={store}>
+    <App />
+    </Provider>
+, rootElement);
